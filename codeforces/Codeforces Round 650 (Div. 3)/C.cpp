2763a681e902f16cc6344716d1df6e80d47375f8
unknown <comihanks@outlook.com>
2026-03-25 20:31:18 +0800

    solve CF 1367C

---
+++ codeforces/Codeforces Round 650 (Div. 3)/C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt; cin>>tt;
+    while(tt--){
+        int n,k; cin>>n>>k;
+        string s; cin>>s;
+        int ans=0;
+        int m=k;
+        while(m--){
+            s=s+'0';
+            s='0'+s;
+        }
+        s=s+'1';
+        s='1'+s;
+        int l=0;
+        int r=0;
+        int len=s.size();
+        while(r<len){
+            if(l==r) ++r;
+            if(r>=len) break;
+            while(s[r]=='0') ++r;
+            ans+=(r-l)/(k+1)-1;
+            l=r;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
